4a75acaea416474a08aab05a514144981fec685c
Jakub Tomczak <jakub.tomczak@live.com>
2025-06-30 22:13:12 +0000

    Set trtllm commit (v0.20)

--- container/build.sh
+++ container/build.sh
@@ -88,7 +88,7 @@ TENSORRTLLM_PIP_WHEEL_DIR="/tmp/trtllm_wheel/"
 # TensorRT-LLM commit to use for building the trtllm wheel if not provided.
 # Important Note: This commit is not used in our CI pipeline. See the CI
 # variables to learn how to run a pipeline with a specific commit.
-TRTLLM_COMMIT="137fe35539ea182f1495f5021bfda97c729e50c3"
+TRTLLM_COMMIT="7965842954628b0b5456a2f7d59786d3dcd41647" # V0.20
 
 # TensorRT-LLM PyPI index URL
 TENSORRTLLM_INDEX_URL="https://pypi.python.org/simple"
